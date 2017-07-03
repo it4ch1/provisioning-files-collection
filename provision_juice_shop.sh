81d7480c48ef56d65a58f699503ea945166b2146
it4ch1 <antopic88@gmail.com>
2017-07-04 01:24:45 +0200

    adding Juice Shop provision file

---
+++ provision_juice_shop.sh
@@ -0,0 +1,12 @@
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+apt-get install -y nodejs php libapache2-mod-php apache2 git
+
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-engine
+
+a2enmod proxy_http
+systemctl restart apache2
+
+docker run --restart=always -d -p 3000:3000 bkimminich/juice-shop
